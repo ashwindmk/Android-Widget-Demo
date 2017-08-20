e9dffdb262e9981fd477cc6695d422bb94a25faa
Ashwin <ashwin.dinesh01@gmail.com>
2017-08-20 16:00:57 +0530

    Removed unnecessary code

--- app/src/main/java/com/ashwin/example/widgetdemo/MainActivity.java
+++ app/src/main/java/com/ashwin/example/widgetdemo/MainActivity.java
@@ -1,30 +1,12 @@
 package com.ashwin.example.widgetdemo;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = MainActivity.class.getSimpleName();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        if (savedInstanceState != null) {
-            Log.d("debuglogging", TAG + " : saved instance state : " + savedInstanceState.toString());
-        } else {
-            Log.d("debuglogging", TAG + " : saved instance state : null");
-        }
-
-        Bundle bundle = getIntent().getExtras();
-        if (bundle != null) {
-            Log.d("debuglogging", TAG + " : intent : " + bundle.toString());
-        } else {
-            Log.d("debuglogging", TAG + " : intent : null");
-        }
     }
 }
